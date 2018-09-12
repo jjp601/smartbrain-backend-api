3a9c2b540a9079c189a6eab99addfd69170ad538
Jon Powell <jpowell68601@gmail.com>
2018-09-11 21:28:05 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SmartBrain API
+
+An Api for a facial recognition app using Clarifai.
---
+++ server.js
@@ -0,0 +1,44 @@
+require('dotenv').config();
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
+
+const db = knex({
+    client: 'pg',
+    connection: {
+      host : process.env.DB_HOST,
+      user : process.env.DB_USER,
+      password : process.env.DB_PASS,
+      database : 'smartbrain_db'
+    }
+});
+
+const app = express();
+app.use(bodyParser.json())
+app.use(cors())
+
+
+app.get('/', (req, res) => {
+    res.send(database.users)
+})
+
+app.post('/signin', (req, res) => { signin.handleSignin(req, res, db, bcrypt) })
+
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
+
+app.get('/profile/:id', (req, res) => { profile.handleProfile(req, res, db) })
+
+app.put('/image', (req, res) => { image.handleImage(req, res, db) })
+app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) })
+
+app.listen(3001, () => {
+    console.log('App is running on port 3001.')
+})
